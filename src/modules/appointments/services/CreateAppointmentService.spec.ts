21028fed448d9388980d9fbb393c47cbb5a1a543
Vítor Cardoso Bertolucci <vitorbertolucci@gmail.com>
2020-12-08 13:43:06 -0300

    Altera estrutura para seguir DDD

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,16 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  // it("should not be able to create two appointments on the same time", () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+});
