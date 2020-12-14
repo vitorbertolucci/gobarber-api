6b304cd400e09343e82f0dd2a61b04f81b557bf9
Vítor Cardoso Bertolucci <vitorbertolucci@gmail.com>
2020-12-14 18:00:38 -0300

    Implementa celebrate e dotenv

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
@@ -13,7 +14,21 @@ const providerDayAvailabilityController = new ProviderDayAvailabilityController(
 providersRoute.use(ensureAuthenticated);
 
 providersRoute.get('/', providersController.index);
-providersRoute.get('/:provider_id/month-availability', providerMonthAvailabilityController.index);
-providersRoute.get('/:provider_id/day-availability', providerDayAvailabilityController.index);
+providersRoute.get('/:provider_id/month-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    }
+  }),
+  providerMonthAvailabilityController.index
+);
+providersRoute.get('/:provider_id/day-availability',
+  celebrate({
+    [Segments.PARAMS]: {
+      provider_id: Joi.string().uuid().required(),
+    }
+  }),
+  providerDayAvailabilityController.index
+);
 
 export default providersRoute;
